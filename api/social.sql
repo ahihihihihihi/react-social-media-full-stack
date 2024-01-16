76011fcbcd3e7bb635eb0fa130f3d4225d10f34d
ahihi <bdsthocu20@gmail.com>
2024-01-16 14:38:22 +0700

    #5

--- api/social.sql
+++ api/social.sql
@@ -43,7 +43,7 @@ CREATE TABLE `Posts` (
   PRIMARY KEY (`id`),
   KEY `userId` (`userId`),
   CONSTRAINT `Posts_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `Users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS `Relationships`;
 CREATE TABLE `Relationships` (
@@ -92,7 +92,9 @@ INSERT INTO `Posts` (`id`, `desc`, `img`, `userId`, `createdAt`) VALUES
 INSERT INTO `Posts` (`id`, `desc`, `img`, `userId`, `createdAt`) VALUES
 (3, 'desc4', NULL, 4, NULL);
 INSERT INTO `Posts` (`id`, `desc`, `img`, `userId`, `createdAt`) VALUES
-(4, 'post from test2 from postman', NULL, 2, '2024-01-16 11:22:39');
+(4, 'post from test2 from postman', NULL, 2, '2024-01-16 11:22:39'),
+(5, 'ahihi', NULL, 2, '2024-01-16 14:28:01'),
+(6, 'ahaha', NULL, 2, '2024-01-16 14:37:09');
 
 INSERT INTO `Relationships` (`id`, `followerUserId`, `followedUserId`) VALUES
 (1, 2, 4);
